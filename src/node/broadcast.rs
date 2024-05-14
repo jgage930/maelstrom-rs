df5e9723f25058eabfed192f83eb0d7be37c11c5
Jack Gage <jgage930@gmail.com>
2024-05-14 11:04:55 -0500

    set up rpcs

---
+++ src/node/broadcast.rs
@@ -0,0 +1,36 @@
+use super::traits::Node;
+use crate::{Body, Message};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub struct BroadcastNode {
+    /// Store the messages this node has seen.
+    message_ids: Vec<usize>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum BroadcastPayload {
+    Broadcast(Broadcast),
+    BroadcastOk,
+    Read,
+    ReadOk(ReadOk),
+    Topology(Toplogy),
+    TopologyOk,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BroadCast {
+    pub message: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ReadOk {
+    pub messages: Vec<usize>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Topology {
+    pub toplogy: HashMap<String, Vec<String>>,
+}
